1226b1d339d124e204967e48d0fd2edd7b3e1541
Ennio Villa <enniosouzavilla@gmail.com>
2024-04-25 20:05:34 +0000

    Create a function to display the word with guessed letters and underscores

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def choose_word():
+    """
+    Function to choose a random word from the list
+    """
+    words = ['apple', 'banana', 'orange', 'grape', 'strawberry', 'melon']
+    return random.choice(words)
+
+def display_word(word, guessed_letters):
+    """
+    Function to display the word with guessed letters and underscores
+    """
+    display = ''
+    for letter in word:
+        if letter in guessed_letters:
+            # If the letter has been guessed, show the letter followed by a space
+            display += letter + ' '
+        else:
+            # If the letter hasn't been guessed, show an underscore followed by a space
+            display += '_ '
+    return display.strip()  # Remove trailing space at the end
