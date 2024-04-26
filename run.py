bdda8473557bbf400102176f115111ec69c7e83a
Ennio Villa <enniosouzavilla@gmail.com>
2024-04-26 09:59:39 +0000

    Create the main game function

--- run.py
+++ run.py
@@ -88,3 +88,55 @@ def display_hangman(attempts):
     ]
     
     return stages[attempts]
+
+
+def hangman():
+    """
+    Main game function
+    """
+    word = choose_word()
+    guessed_letters = []
+    attempts = 6
+
+    print('Welcome to The Hangman Game!')
+    print(display_hangman(attempts))
+    print(display_word(word, guessed_letters))
+
+    while True:
+        guess = input('Guess a letter: ').lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            # Check if input is a single letter
+            print('Please enter a single letter.')
+            continue
+
+        if guess in guessed_letters:
+            # Check if the letter has already been guessed
+            print('You have already guessed that letter.')
+            continue
+
+        guessed_letters.append(guess)
+
+        if guess not in word:
+            # If the guessed letter is not in the word, decrement attempts
+            attempts -= 1
+            print(f'Incorrect! You have {attempts} attempts left.')
+            print(display_hangman(attempts))
+            if attempts == 0:
+                # If no attempts left, end the game
+                print("You've run out of attempts. Game over!")
+                print(f'The word was: {word}')
+                break
+        else:
+            print('Correct guess!')
+
+        display = display_word(word, guessed_letters)
+        print(display)
+
+        if "_" not in display:
+            # If no underscores left, player wins
+            print('You have guessed the word correctly. Congratulations!')
+            break
+
+
+hangman()
